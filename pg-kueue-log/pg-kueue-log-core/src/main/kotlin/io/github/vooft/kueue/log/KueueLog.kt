3adaa976b77b3490b992e602e7f9f67ce0c95bee
vooft <52610+vooft@users.noreply.github.com>
2024-08-15 21:34:08 +0400

    Added KueueLog starting point

--- pg-kueue-log/pg-kueue-log-core/src/main/kotlin/io/github/vooft/kueue/log/KueueLog.kt
+++ pg-kueue-log/pg-kueue-log-core/src/main/kotlin/io/github/vooft/kueue/log/KueueLog.kt
@@ -2,8 +2,9 @@ package io.github.vooft.kueue.log
 
 import io.github.vooft.kueue.KueueConnection
 import io.github.vooft.kueue.KueueTopic
+import io.github.vooft.kueue.persistence.KueueConsumerGroup
 
 interface KueueLog<C, KC : KueueConnection<C>> {
     suspend fun createProducer(topic: KueueTopic): KueueProducer<C, KC>
-    suspend fun createConsumer(topic: KueueTopic): KueueConsumer
+    suspend fun createConsumer(topic: KueueTopic, group: KueueConsumerGroup): KueueConsumer
 }
--- pg-kueue-log/pg-kueue-log-core/src/main/kotlin/io/github/vooft/kueue/log/impl/KueueLogImpl.kt
+++ pg-kueue-log/pg-kueue-log-core/src/main/kotlin/io/github/vooft/kueue/log/impl/KueueLogImpl.kt
@@ -1,17 +1,33 @@
 package io.github.vooft.kueue.log.impl
 
 import io.github.vooft.kueue.KueueConnection
+import io.github.vooft.kueue.KueueConnectionProvider
 import io.github.vooft.kueue.KueueTopic
 import io.github.vooft.kueue.log.KueueConsumer
 import io.github.vooft.kueue.log.KueueLog
 import io.github.vooft.kueue.log.KueueProducer
+import io.github.vooft.kueue.log.impl.consumer.KueueConsumerDao
+import io.github.vooft.kueue.log.impl.consumer.KueueConsumerImpl
+import io.github.vooft.kueue.log.impl.producer.KueueProducerImpl
+import io.github.vooft.kueue.persistence.KueueConsumerGroup
+import io.github.vooft.kueue.persistence.KueueConsumerMessagePoller
+import io.github.vooft.kueue.persistence.KueuePersister
 
-class KueueLogImpl<C, KC : KueueConnection<C>> : KueueLog<C, KC> {
+class KueueLogImpl<C, KC : KueueConnection<C>>(
+    private val connectionProvider: KueueConnectionProvider<C, KC>,
+    private val persister: KueuePersister<C, KC>,
+    private val poller: KueueConsumerMessagePoller
+) : KueueLog<C, KC> {
     override suspend fun createProducer(topic: KueueTopic): KueueProducer<C, KC> {
-        TODO("Not yet implemented")
+        return KueueProducerImpl(topic = topic, connectionProvider = connectionProvider, persister = persister)
     }
 
-    override suspend fun createConsumer(topic: KueueTopic): KueueConsumer {
-        TODO("Not yet implemented")
+    override suspend fun createConsumer(topic: KueueTopic, group: KueueConsumerGroup): KueueConsumer {
+        return KueueConsumerImpl(
+            topic = topic,
+            consumerGroup = group,
+            consumerDao = KueueConsumerDao(connectionProvider, persister),
+            poller = poller
+        )
     }
 }
